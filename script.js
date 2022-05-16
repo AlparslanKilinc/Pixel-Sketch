120b02b04159f84315a8764b37f3cc881e011c5a
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-05-16 13:13:32 -0400

    add size button functions & clear function, color picker

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 // Defaults
-let size=16;
 
 
 
 // get Html elements 
 const grid=document.querySelector('#grid');
 
-makeGrid(size,grid);
+/// Size Options
+ const Sixteen = document.querySelector('.Sixteen');
+ const Thirty_two = document.querySelector('.Thirty_two');
+ const Sixty_four = document.querySelector('.Sixty_four');
+
+ //// Side Options 
+
+ const color = document.querySelector('#picker');
+ const eraser = document.querySelector('.eraser');
+ const rainbow = document.querySelector('.rainbow');
+ const clear = document.querySelector('.clear');
+
+
+
+
+function clearUp(){
+  const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box =>{
+      box.remove();
+    });
+}
+
+
 
 function makeGrid(size,grid){
-    
     grid.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows= `repeat(${size}, 1fr)`;
-
     for(let i=0; i<size*size; i++){
       let box =document.createElement('div');
       box.classList.add('box');
@@ -20,5 +39,51 @@ function makeGrid(size,grid){
       box.style.border='2px solid black';
       grid.appendChild(box);
     }
-
 }
+
+//// Side Option Buttons 
+
+color.addEventListener('change' , (e)=>{
+  
+  console.log(e.target.value);
+
+});
+
+
+
+
+
+
+clear.addEventListener('click' , ()=>{
+  clearUp();
+});
+
+
+
+
+
+//// Button Actions For Size 
+
+  Sixteen.addEventListener('click' ,()=>{
+    // Clear Grid 
+    let g = grid;
+    clearUp();
+    // Make new grid size
+    makeGrid(16,g);
+  });
+
+  Thirty_two.addEventListener('click' ,()=>{
+    // Clear Grid 
+    let g = grid;
+    clearUp();
+    // Make new grid size
+    makeGrid(32,g);
+  });
+
+  Sixty_four.addEventListener('click' ,()=>{
+    // Clear Grid 
+    let g = grid;
+    clearUp();
+    // Make new grid size
+    makeGrid(64,g);
+  });
