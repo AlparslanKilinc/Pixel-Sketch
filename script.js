44895780e3794e8ee9a5ae9008048150449b9534
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-08-04 18:06:20 -0400

    organize html & js code, get rid of unessecary spacing group js functions accordingly, change functionality to click and drag to draw.

--- script.js
+++ script.js
@@ -1,46 +1,31 @@
   /// Default
+  window.onload = () => {makeGrid(16);}
   let color = 'black';
-  let click = false;
-  let off=false;
+  let BorderChanges=false;
+  let click = false
+  document.body.onmousedown = () => (click = true)
+  document.body.onmouseup = () => (click = false)
 
+  function makeGrid(size){
+    let grid=document.querySelector('#grid');
+    let boxes= grid.querySelectorAll('div');
+    /// clear grid when making a new one
+    boxes.forEach((div) => div.remove());
+    /// set column/row of the grid
+    grid.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows= `repeat(${size}, 1fr)`;
   
-
-
-  //// Side Options 
-  const picker = document.querySelector('#picker');
-  picker.addEventListener('change' , (e)=>{
-   color = e.target.value;
-   });
- 
- 
- //// Functions 
- function makeGrid(size){
-  let grid=document.querySelector('#grid');
-  let boxes= grid.querySelectorAll('div');
-  
-   /// clear grid when making a new one
-   boxes.forEach((div) => div.remove());
-   /// set column/row of the grid
-   grid.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
-   grid.style.gridTemplateRows= `repeat(${size}, 1fr)`;
- 
-   for(let i=0; i<size*size; i++){
-     let box = document.createElement('div');
-     box.addEventListener('mouseover',draw);
-     box.style.backgroundColor='white';
-     if(off) box.style.border='0px';
-     else box.style.border='1px solid black';
-     grid.insertAdjacentElement('beforeend',box);
-   }
-   
-   
+    for(let i=0; i<size*size; i++){
+      let box = document.createElement('div');
+      box.addEventListener('mouseover', draw);
+      box.addEventListener('mousedown', draw);
+      box.style.backgroundColor='white';
+      if(BorderChanges) box.style.border='0px';
+      else box.style.border='1px solid black';
+      grid.insertAdjacentElement('beforeend',box);
+    } 
  }
- 
- /// Default Run 
- window.onload = () => {
-   makeGrid(16);
- }
- 
+
  function draw (){
    if(click){
      if(color==='rainbow') {
@@ -52,11 +37,12 @@
    }
  }
 
+  //// Side Options 
  function BorderChange(){
   let grid=document.querySelector('#grid');
   let boxes= grid.querySelectorAll('div');
-    off = !off;
-  if(off)boxes.forEach((div) => div.style.border='0px');
+  BorderChanges = !BorderChanges;
+  if(BorderChanges)boxes.forEach((div) => div.style.border='0px');
   else boxes.forEach((div) => div.style.border='1px solid black');
   }
  
@@ -65,11 +51,14 @@
    let boxes = grid.querySelectorAll('div');
    boxes.forEach((div) => (div.style.backgroundColor='white'));
  }
- 
- function changeColor(input){
-   color = input;
- }
- 
+
+ function changeColor(input){color=input;}
+
+ const picker = document.querySelector('#picker');
+ picker.addEventListener('change' , (e)=>{
+  color = e.target.value;
+  });
+
  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
  function getRainbow(){
    const r = randomBetween(0, 255);
@@ -78,18 +67,3 @@
    let color = `rgb(${r},${g},${b})`;
    return color;
  }
- 
-   document.querySelector('#grid').addEventListener('click' , () => click=!click);
-
-
-
-
-
-
-
-
-
-
-
-
-
