01735b9a145b823f8b249688d4a1f7db7bba1baa
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-01-08 14:49:33 -0500

    touch screen draw action

--- script.js
+++ script.js
@@ -5,29 +5,52 @@
   let click = false;
   document.body.onmousedown = () => (click = true);
   document.body.onmouseup = () => (click = false);
-  document.addEventListener("touchstart",()=>{click=true});
-  document.addEventListener("touchend",()=>{click=false});
+
+  function getPosition(element) {
+    let xPosition = 0;
+    let yPosition = 0;
+    while (element) {
+       xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+       yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+       element = element.offsetParent;
+    }
+    return { x: xPosition, y: yPosition };
+}
  
 
   function makeGrid(size){
     let grid=document.querySelector('#grid');
+      //// Touch Move 
+      grid.addEventListener("touchmove", event =>{
+        event.preventDefault();
+        let myLocation = event.touches[0];
+        let realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
+        let y = getPosition(document.getElementById('grid')).y;
+        let x = getPosition(document.getElementById('grid')).x;
+        console.log(y+grid.clientHeight);
+        console.log(x+grid.clientWidth);
+        if(x+grid.clientWidth>myLocation.clientX &&  myLocation.clientX>x  && y+grid.clientHeight>myLocation.clientY && myLocation.clientY >y)
+          if(color==='rainbow') {
+            let rainbow = getRainbow();
+            realTarget.style.backgroundColor=rainbow;
+            }else{
+            realTarget.style.backgroundColor = color;
+            }
+         });
     let boxes= grid.querySelectorAll('div');
     /// clear grid when making a new one
     boxes.forEach((div) => div.remove());
     /// set column/row of the grid
     grid.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows= `repeat(${size}, 1fr)`;
-  
     for(let i=0; i<size*size; i++){
       let box = document.createElement('div');
       box.addEventListener('mousemove', draw);
-      box.addEventListener("touchmove", draw);
       box.style.backgroundColor='white';
       if(BorderChanges) box.style.border='0px';
       else box.style.border='1px solid black';
       grid.insertAdjacentElement('beforeend',box);
-    } 
-    
+    }
  }
 
  function draw (event){
